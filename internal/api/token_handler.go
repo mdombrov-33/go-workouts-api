1142b85bec1166dbf06d71b1abede4da8fd0760b
Maksym Dombrov <maksymdombrov@gmail.com>
2025-09-10 17:19:44 +0300

    finish auth

---
+++ internal/api/token_handler.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/mdombrov-33/go-api-fm/internal/store"
+	"github.com/mdombrov-33/go-api-fm/internal/tokens"
+	"github.com/mdombrov-33/go-api-fm/internal/utils"
+)
+
+type TokenHandler struct {
+	tokenStore store.TokenStore
+	userStore  store.UserStore
+	logger     *log.Logger
+}
+
+type createTokenRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func NewTokenHandler(tokenStore store.TokenStore, userStore store.UserStore, logger *log.Logger) *TokenHandler {
+	return &TokenHandler{
+		tokenStore: tokenStore,
+		userStore:  userStore,
+		logger:     logger,
+	}
+}
+
+func (h *TokenHandler) HandleCreateToken(w http.ResponseWriter, r *http.Request) {
+	var req createTokenRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+
+	if err != nil {
+		h.logger.Printf("ERROR: createTokenRequest: %v", err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error": "invalid request body"})
+		return
+	}
+
+	// get the user
+	user, err := h.userStore.GetUserByUsername(req.Username)
+	if err != nil || user == nil {
+		h.logger.Printf("ERROR: get user by username: %v", err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "internal server error"})
+		return
+	}
+
+	passwordsDoMatch, err := user.PasswordHash.Matches(req.Password)
+	if err != nil {
+		h.logger.Printf("ERROR: checking password hash: %v", err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "internal server error"})
+		return
+	}
+
+	if !passwordsDoMatch {
+		utils.WriteJSON(w, http.StatusUnauthorized, utils.Envelope{"error": "invalid credentials"})
+		return
+	}
+
+	token, err := h.tokenStore.CreateNewToken(user.ID, 24*time.Hour, tokens.ScopeAuth)
+	if err != nil {
+		h.logger.Printf("ERROR: creating new token: %v", err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "internal server error"})
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, utils.Envelope{"auth_token": token})
+}
